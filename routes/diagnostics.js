2456825a4656c377ec3b8958eb6dd3c559a05518
axeljk <axeljkern@yahoo.com>
2022-07-25 14:35:24 -0400

    Adds diagnostics database backend.

--- routes/diagnostics.js
+++ routes/diagnostics.js
@@ -5,11 +5,18 @@ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 // GET Route for retrieving diagnostic information
 diagnostics.get('/', (req, res) => {
   // TODO: Logic for sending all the content of db/diagnostics.json
+	readFromFile("./db/diagnostics.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a error logging
 diagnostics.post('/', (req, res) => {
   // TODO: Logic for appending data to the db/diagnostics.json file
+	let newEntry = {};
+	newEntry.time = new Date().getTime();
+	newEntry.error_id = uuidv4();
+	newEntry.errors = req.body.errors;
+	readAndAppend(newEntry, "./db/diagnostics.json");
+	readFromFile("./db/diagnostics.json").then((data) => res.json(JSON.parse(data)));
 });
 
 module.exports = diagnostics;
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const express = require('express');
 const tipsRouter = require('./tips');
 const feedbackRouter = require('./feedback');
 // TODO: import your diagnostics route
+const diagnosticsRouter = require("./diagnostics");
 
 const app = express();
 
 app.use('/tips', tipsRouter);
 app.use('/feedback', feedbackRouter);
 // TODO: Initialize diagnostics route
+app.use("/diagnostics", diagnosticsRouter);
+
 
 module.exports = app;
