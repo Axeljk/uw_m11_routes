0f0f4ca8f368dd44d85b232edee8e524fbd53db6
axeljk <axeljkern@yahoo.com>
2022-07-25 14:34:01 -0400

    Adds 404 page to page requests that don't exist.

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
+// GET Route for 404 page.
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "/public/pages/404.html")));
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
